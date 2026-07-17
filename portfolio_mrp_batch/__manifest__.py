7657bd7e79e9146b38437a9d60bb6a8b906a6b50
ahmed anter <anterahmed818@gmail.com>
2026-07-17 05:29:26 +0300

    Add portfolio_mrp_batch/__manifest__.py

---
+++ portfolio_mrp_batch/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Portfolio MRP Batch Scaling",
+    "version": "18.0.1.0.0",
+    "category": "Manufacturing",
+    "summary": "Scale manufacturing quantities using batch and machine factors",
+    "license": "LGPL-3",
+    "depends": ["mrp"],
+    "data": ["views/mrp_production_views.xml"],
+    "installable": True,
+    "application": False,
+}
